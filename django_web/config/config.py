c8db43c7c9094bdcf858e689a0151ee5b18aef23
Egor Ivanov <rust_lang_lover@proton.me>
2025-04-24 14:59:54 +0300

    updated

--- django_web/config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    def __init__(self):
-        self.SENTRY_DSN = os.getenv("SENTRY_DSN")
-        self.DJANGO_SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-        self.DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
--- my_blog_django/my_blog_django/urls.py
+++ my_blog_django/my_blog_django/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for django_web project.
+URL configuration for my_blog_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,17 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
-from django.urls import path, include
-
-
-def trigger_error(request):
-    _ = 1 / 0
-
+# from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("orders_client/", include("orders_client.urls")),
-    path("sentry-debug/", trigger_error),
+    #    path('admin/', admin.site.urls),
 ]
